24a92740aefc4ffd0e7e22918ceb9986873114a8
Zeratul352 <43727651+Zeratul352@users.noreply.github.com>
2020-10-21 15:32:51 +0300

    Added binaries solution

---
+++ Binaries.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binare_task_algorythm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 254;
+            int onescount = 0;
+            while(num > 0)
+            {
+                if((num>>1)<<1 != num)
+                {
+                    onescount++;
+                }
+                num = num >> 1;
+            }
+            int number = 0;
+            
+            for(int i = 0; i < onescount; i++)
+            {
+                number = (number << 1) + 1;
+            }
+            int lownum = number;
+            int highnum = number;
+            int rasryady = 16;
+            highnum = highnum << (rasryady - onescount);
+            Console.WriteLine(highnum);
+            Console.WriteLine(lownum);
+            Console.ReadKey();
+        }
+    }
+}
